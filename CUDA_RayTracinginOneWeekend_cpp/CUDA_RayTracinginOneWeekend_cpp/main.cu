9d0cb27b013c6ee4698ba2071097295b7fc0ff85
haoboxuxu <haoboxuxu@icloud.com>
2021-03-10 10:21:42 +0800

    Chapter 2

--- CUDA_RayTracinginOneWeekend_cpp/CUDA_RayTracinginOneWeekend_cpp/main.cu
+++ CUDA_RayTracinginOneWeekend_cpp/CUDA_RayTracinginOneWeekend_cpp/main.cu
@@ -2,130 +2,86 @@
 //  main.cpp
 //  CUDA_RayTracinginOneWeekend_cpp
 //
-//  Created by ÂæêÊµ©Âçö on 2021/3/8.
+//  Created by –Ï∫∆≤© on 2021/3/8.
 //
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <iostream>
 #include <fstream>
-#include "utilitys.h"
-#include "Color.h"
-#include "hittable_list.h"
-#include "Sphere.h"
-#include "Material.h"
-#include "Camera.h"
-#include "Material.h"
-#include "RenderConfig.h"
-#include <curand_kernel.h>
 using namespace std;
 
-hittable_list random_scene() {
-    hittable_list world;
-    
-    auto ground_material = make_shared<Lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<Sphere>(point3(0,-1000,0), 1000, ground_material));
-    
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-            
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<Material> sphere_material;
-                
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<Lambertian>(albedo);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<Metal>(albedo, fuzz);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
-                } else {
-                    // glass
-                    sphere_material = make_shared<Dielectric>(1.5);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
-                }
-            }
-        }
+// check cuda error
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        cudaDeviceReset();
+        exit(99);
     }
-    
-    auto material1 = make_shared<Dielectric>(1.5);
-    world.add(make_shared<Sphere>(point3(0, 1, 0), 1.0, material1));
-    
-    auto material2 = make_shared<Lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<Sphere>(point3(-4, 1, 0), 1.0, material2));
-    
-    auto material3 = make_shared<Metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<Sphere>(point3(4, 1, 0), 1.0, material3));
-    
-    return world;
 }
 
-__device__ color ray_color(const Ray& r, const hittable& world, int depth) {
-    hit_record rec;
-    
-    // If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0) {
-        return color(0,0,0);
-    }
-    
-    if (world.hit(r, 0.001, infinity, rec)) {
-        Ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-            return attenuation * ray_color(scattered, world, depth-1);
-        }
-        return color(0,0,0);
-    }
-    
-    Vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+__global__ void render(float* fb, int max_x, int max_y) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x * 3 + i * 3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
 }
 
 int main() {
-    ofstream fout("output.ppm");
+    ofstream fout("C:\\Users\\haobo\\Desktop\\output.ppm");
     if (fout.fail()) {
         cout << "open file output.ppm failed" << endl;
         return -1;
     }
-    
-    // Image
-    
-    // World
-    auto world = random_scene();
-    
-    // Camera
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    Vec3 vup(0,1,0);
-    auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
-    
-    Camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
-    
-    // Render
+
+    int image_width = 256;
+    int image_height = 256;
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Rendering a " << image_width << "x" << image_height << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = image_width * image_height;
+    size_t fb_size = 3 * num_pixels * sizeof(float);
+
+    // allocate FB
+    float* fb;
+    checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
+
+    // Render our buffer
+    dim3 blocks(image_width / tx + 1, image_height / ty + 1);
+    dim3 threads(tx, ty);
+    render << <blocks, threads >> > (fb, image_width, image_height);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    // Output FB as Image
     fout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
-                Ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
-            write_color(fout, pixel_color, samples_per_pixel);
+        for (int i = 0; i < image_width; i++) {
+            size_t pixel_index = j * 3 * image_width + i * 3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            fout << ir << " " << ig << " " << ib << "\n";
         }
     }
-    
+
+    checkCudaErrors(cudaFree(fb));
+
     std::cerr << "\nDone.\n";
-    
+
     fout.close();
-    
+
     return 0;
 }
