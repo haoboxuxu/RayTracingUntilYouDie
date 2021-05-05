d8f518e7f4d4e489a83806a8ebfca820c01610da
haoboxuxu <haoboxuxu@icloud.com>
2021-05-05 21:19:46 +0800

    init theNextWeedend

--- RayTracinginOneWeekend_cpp/RayTracinginOneWeekend_cpp/main.cpp
+++ RayTracinginOneWeekend_cpp/RayTracinginOneWeekend_cpp/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  RayTracinginOneWeekend_cpp
+//  RayTracingingTheNextWeek_cpp
 //
 //  Created by 徐浩博 on 2021/2/4.
 //
@@ -62,30 +62,6 @@ hittable_list random_scene() {
     
     return world;
 }
-
-
-// recursiv
-//color ray_color(const Ray& r, const hittable& world, int depth) {
-//    hit_record rec;
-//
-//    // If we've exceeded the ray bounce limit, no more light is gathered.
-//    if (depth <= 0) {
-//        return color(0,0,0);
-//    }
-//
-//    if (world.hit(r, 0.001, infinity, rec)) {
-//        Ray scattered;
-//        color attenuation;
-//        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-//            return attenuation * ray_color(scattered, world, depth-1);
-//        }
-//        return color(0,0,0);
-//    }
-//
-//    Vec3 unit_direction = unit_vector(r.direction());
-//    auto t = 0.5*(unit_direction.y() + 1.0);
-//    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
-//}
 
 // loop
 color ray_color(const Ray& r, const hittable& world, int depth) {
